2f147a5926d0b092f6016e47eed57c1c7467efc2
simone.puce <simone.puce@finconsgroup.com>
2024-02-26 09:11:43 +0100

    temp commit

---
+++ src/components/forms/register/RegisterForm.tsx
@@ -0,0 +1,56 @@
+import { Button, Input } from "antd";
+import Form, { Rule } from "antd/es/form";
+import { ReactElement } from "react";
+import "./RegisterForm.css"
+//import { useNavigate } from "react-router-dom";
+
+const RegisterForm = (): ReactElement => {
+    //const navigate = useNavigate()
+    const passwordRegex: RegExp = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+    const [form] = Form.useForm() //hook antd
+    const passwordValidation = (): boolean => {
+        return form.getFieldValue("password") === form.getFieldValue("confirmPassword") ? true : false;
+    }
+
+    const onSubmit = (): void => {
+        passwordValidation() ? console.log("password match") : console.log("password do not match")
+    }
+
+    const emailRules: Rule[] = [
+        { required: true, message: "Please input your email!", type: "email" }
+    ]
+    const passwordRules: Rule[] = [
+        { required: true, message: "Please input your password!"},
+        { pattern: passwordRegex, message: "Password should have at least a number and a special character" }
+    ]
+
+    return (
+        <div className="register-form-container">
+            <Form
+                form={form}
+                layout="vertical"
+                name="login"
+                initialValues={{ remember: true }}
+                onFinish={onSubmit}
+                //onFinishFailed={}
+                autoComplete="off">
+                <Form.Item label="Email" name="email" rules={emailRules}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Password" name="password" rules={passwordRules}>
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item label="Confirm password" name="confirmPassword" rules={passwordRules}>
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    )
+}
+
+export default RegisterForm;
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/forms/login/LoginForm";
+import "./LoginPage.css"
+
+const LoginPage = () => {
+  return (
+    <div className="login-form-wrapper">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default LoginPage;
