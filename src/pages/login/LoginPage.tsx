1925908a9adb9fbe165275f9f5152200fd1a105a
Michelangelo Masiello <152301894+michelangelomasiello1@users.noreply.github.com>
2024-02-26 15:22:44 +0100

    Design-pages (#4)
    * Publish the branch for design the pages
    
    * Fixed registration form

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,11 +1,17 @@
+import { Layout } from "antd";
 import LoginForm from "../../components/forms/login/LoginForm";
 import "./LoginPage.css"
 
+
+const { Header, Footer, Sider, Content } = Layout;
+
 const LoginPage = () => {
   return (
-    <div className="login-form-wrapper">
+      <div className="login-form-wrapper">
       <LoginForm />
     </div>
+
+
   )
 }
 
