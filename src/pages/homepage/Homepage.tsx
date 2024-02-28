897fbadecdb6e34fe57f945b814fa475460a349c
simone.puce <simone.puce@finconsgroup.com>
2024-02-28 17:34:22 +0100

    ADDED SPACING

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -17,6 +17,11 @@ const Homepage = () => {
             <Footer style={{ textAlign: 'center' }}>
                 Ant Design ©{new Date().getFullYear()} Created by Ant UED
             </Footer>
+
+
+
+
+            
         </Layout>
     )
 }
