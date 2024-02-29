d66d72b419b1f094c6cbb3dd836aee4b1ae623a4
michelangelo.masiell <michelangelo.masiello@finconsgroup.com>
2024-02-29 15:38:47 +0100

    succesfully registered modal implemented

--- src/components/forms/register/RegisterForm.tsx
+++ src/components/forms/register/RegisterForm.tsx
@@ -1,35 +1,51 @@
-import { Button, Input, message, notification } from "antd";
-import Form, { Rule } from "antd/es/form";
-import { ReactElement, useState } from "react";
+import { Button, Input, Popover, message } from "antd"
+import Form, { Rule } from "antd/es/form"
+import { ReactElement, useState } from "react"
 import "./RegisterForm.css"
-import { Link } from "react-router-dom";
-import { registerNewUser } from "../../../services/UserService";
-import SuccessfulRegisterUserModal from "../../modals/SuccessfulRegisterUserModal";
+import { Link, useNavigate } from "react-router-dom"
+import { registerNewUser } from "../../../services/UserService"
+import SuccessfulRegisterUserModal from "../../modals/SuccessfulRegisterUserModal"
 
 const RegisterForm = (): ReactElement => {
     const passwordRegex: RegExp = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
     const [form] = Form.useForm()
     const [messageApi, contextHolder] = message.useMessage();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [clicked, setClicked] = useState(false);
+    const popOverMessage: string = "password do not match";
+    const navigate = useNavigate();
     const passwordValidation = (): boolean => {
         return form.getFieldValue("password") === form.getFieldValue("confirmPassword") ? true : false;
     }
- const showModal = (): void => {
-    setIsModalOpen(true);
-  };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-
-    const showPasswordInfo = () => {
-        messageApi.info('Password should have a capital letter, a number and a special character');
+    const clickedManagment = () => {
+        setClicked(true)
+        setTimeout(()=>{
+            setClicked(false)
+        },1500)
     }
+
+    const handleClickChange = () => {
+        passwordValidation() ? setClicked(false) : clickedManagment()
+    }
+
+
+    const showModal = (): void => {
+        setIsModalOpen(true);
+    }
+
+    const handleOk = () => {
+        navigate("/")
+    }
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    }
+
+
+    /*const showPasswordInfo = () => {
+        messageApi.info('Password should have a capital letter, a number and a special character');
+    }*/
 
     const onFailedSubmit = () => {
         messageApi.warning('Fill the form properly');
@@ -39,14 +55,23 @@ const RegisterForm = (): ReactElement => {
     const sendData = async () => {
         const { email, firstName, lastName, password } = form.getFieldsValue(["email", "firstName", "lastName", "password"])
         const response = await registerNewUser({ email, firstName, lastName, password })
-        response.success === true ? showModal() : message.error("CARLO")
+        response.success === true ? showModal() : message.error("This user already exists ")
+    }
+
+    const titleFunction = (): string => {
+        passwordValidation() === true ? setClicked(false) : setClicked(true)
+        return popOverMessage
     }
 
 
-    const onSubmit = (): void => {
-        passwordValidation() ? sendData() : message.error("The password doesn't match")
-        
+    const onSubmit = (): any => {
+        if (passwordValidation() === true) {
+            sendData()
+        } else {
+
+        }
     }
+
 
     const emailRules: Rule[] = [
         { required: true, message: "Please input a valid email!", type: "email" }
@@ -61,15 +86,14 @@ const RegisterForm = (): ReactElement => {
         { required: true, message: "Please repeat your password!", pattern: passwordRegex }
     ]
 
-   
 
     return (
         <div className="register-form-container">
             <SuccessfulRegisterUserModal
-            showModal = {showModal}
-            isModalOpen = {isModalOpen}
-            handleOk = {handleOk}
-            handleCancel = {handleCancel}
+                showModal={showModal}
+                isModalOpen={isModalOpen}
+                handleOk={handleOk}
+                handleCancel={handleCancel}
             />
             {contextHolder}
             <Form
@@ -90,17 +114,30 @@ const RegisterForm = (): ReactElement => {
                 <Form.Item label="Last name" name="lastName" rules={anagraphicRules}>
                     <Input />
                 </Form.Item>
-                <Form.Item label="Password" name="password" rules={passwordRules}>
-                    <Input.Password onFocus={showPasswordInfo} />
-                </Form.Item>
+                <Popover
+                    placement="bottom"
+                    content={"Password should have a capital letter, a number and a special character"}
+                    trigger="click" >
+                    <Form.Item label="Password" name="password" rules={passwordRules}>
+                        <Input.Password /*onFocus={showPasswordInfo} */ />
+                    </Form.Item>
+                </Popover>
                 <Form.Item label="Confirm password" name="confirmPassword" rules={confirmPasswordRules}>
                     <Input.Password />
                 </Form.Item>
                 <div className="submit-login">
                     <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Register
-                        </Button>
+                        <Popover
+                            placement="left"
+                            title={titleFunction}
+                            trigger="click"
+                            open={clicked}
+                            onOpenChange={handleClickChange}
+                        >
+                            <Button type="primary" htmlType="submit">
+                                Register
+                            </Button>
+                        </Popover>
                     </Form.Item>
                     <Form.Item>
                         <Link to="/">Login</Link>
--- src/components/modals/SuccessfulRegisterUserModal.tsx
+++ src/components/modals/SuccessfulRegisterUserModal.tsx
@@ -2,15 +2,30 @@ import React, { ReactElement, useState } from 'react';
 import { Button, Modal } from 'antd';
 import { ISuccessRegistrationModal } from '../../interfaces/components/modal/ISuccessRegistrationModal';
 import "./SuccessfulRegisterUserModal.css"
+import { Link, useNavigate } from 'react-router-dom';
 
-const SuccessfulRegisterUserModal = ({showModal, isModalOpen, handleOk, handleCancel}: ISuccessRegistrationModal): ReactElement => {
+const SuccessfulRegisterUserModal = ({ showModal, isModalOpen, handleOk, handleCancel }: ISuccessRegistrationModal): ReactElement => {
+
+    const navigate = useNavigate()
 
   return (
     <>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-       <div className='modalStyle'>
-            CARLO
-       </div>
+      <Modal title="Register successfully"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        okText="Login Page"
+        cancelText="New Account"
+        className='succesRegistrationModalStyle'
+        footer={
+          <div className='footer-div'>
+            <Button type="primary" className='login-button' onClick={() => navigate("/login")}>Login</Button>
+            <Button type="link" className='newUser-button' onClick={handleCancel}>New User</Button>
+          </div>
+        }
+      >
+        <div className='succesRegistrationModalStyle'>
+          You are now registered on this site
+        </div>
       </Modal>
     </>
   );
